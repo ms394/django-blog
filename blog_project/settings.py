ba73f4a1c75d762141dee5916c91b5cde7ca7b56
ms394 <mohsinsajan394@gmail.com>
2021-02-17 22:28:18 +0530

    Minor Fixes

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'blog',
     'accounts'
 ]
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
